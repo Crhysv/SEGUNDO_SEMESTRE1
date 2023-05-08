805e462115c0f02375423f6adf646b8569d3db17
Crhys <crhystel.velasco@udla.edu.ec>
2023-05-08 15:41:47 -0500

    este te invierte el nombre

--- nuevo.c
+++ nuevo.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     char nombre[60];
+    int i;
     printf("Ingrese su nombre\n");
     scanf("%[^\n]", nombre);
-    printf("Welcome %s", nombre);
+    printf("Welcome %s\n", nombre);
+    for (i=strlen(nombre)-1; i>=0; i--){
+        printf(" %c", nombre[i]);
+    }
 }
