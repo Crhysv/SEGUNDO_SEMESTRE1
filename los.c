60b33e97588f8514b1ae9fc4755e88f4569c1634
Crhysss <crhystel.velasco@udla.edu.ec>
2023-05-15 15:31:59 -0500

    intento 1 corrigelo despues

--- los.c
+++ los.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main (){
-    printf("HELEKj");
-    printf("How");
+    int candidato[5];
+    int voto=0, i;
+    while(voto!=0){
+        printf("Ingrese el numero del candidato para registrar el voto, 0 para terminar\n");
+        scanf("%d", &voto);
+        if(voto>=1 || voto<=5){
+            candidato[voto-1]++;  
+        }
+        else{
+            printf("Candidato no registrado");
+        }
+        
 
+    }
+    printf("Votos de candidato 1: %d  Votos candidato 2: %d", candidato [1], candidato[2]);
     return 0;
+
 }
