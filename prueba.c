d3e99f53b323d3d5c4531f81874eef45cef03039
Crhysv <122888218+Crhysv@users.noreply.github.com>
2023-10-25 21:48:15 -0500

    Add files via upload

---
+++ prueba.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+    int x,y, n, sum;
+    while (n!=0){
+        printf("Ingrese un numero entero para realizar la suma");
+        scanf("%d", &n);
+        if (n>0 && sum<1200){
+            sum=n+n;
+        }
+        
+    }
+   
+    
+
+
+    
+    
+    return 0;
+}
+
